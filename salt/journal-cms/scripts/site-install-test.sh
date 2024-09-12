6af953c5b5a14279bc9ec01fedc4b930ca9e1b6e
Scott Aubrey <scott@aubrey.org.uk>
2024-09-12 11:10:43 +0100

    change site-install-test.sh to include demo environment logic

--- salt/journal-cms/scripts/site-install-test.sh
+++ salt/journal-cms/scripts/site-install-test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 ENVIRONMENT=$1
 if [[ $ENVIRONMENT == "ci" || $ENVIRONMENT == "dev" ]]; then
-    echo "Always site-install" 
+    echo "Always site-install"
     exit 0
 fi
-# if starts with demo and first previsioned
-#     echo "always site-install"
-#     exit 0
-# fi
-exit 0 
+if [[ $ENVIRONMENT == demo* ]]; then
+    echo "always site-install on first provision"
+    exit 0
+fi
+exit 0
