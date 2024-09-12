8257e9b4aa6492e08b92f156c23f44eb87e33899
Nathan Lisgo <nathan@lisgo.co.uk>
2024-09-12 11:19:17 +0100

    Add first provision check to site-install-test.sh
    Added a function 'is_first_provision' to check if the provision is the first one. Only run site-install on the first provision for demo environments.

--- salt/journal-cms/scripts/site-install-test.sh
+++ salt/journal-cms/scripts/site-install-test.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 ENVIRONMENT=$1
+
+is_first_provision() {
+    return 1
+}
+
 if [[ $ENVIRONMENT == "ci" || $ENVIRONMENT == "dev" ]]; then
     echo "Always site-install"
     exit 0
 fi
-if [[ $ENVIRONMENT == demo* ]]; then
+if [[ $ENVIRONMENT == demo* ]] && is_first_provision; then
     echo "always site-install on first provision"
     exit 0
 fi
