3d8f62d8b8966bfe015dbddcd7ff346285a3f0b2
Daniel Haarhoff <daniel@rknt.de>
2024-09-12 11:34:01 +0100

    Add a test for first provisioned using the existence of a filepath elifesciences/issues#8678

--- salt/journal-cms/scripts/site-install-test.sh
+++ salt/journal-cms/scripts/site-install-test.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+set -euo pipefail
+
 ENVIRONMENT=$1
+FIRST_PROVISIONED=$2
 
 is_first_provision() {
-    return 1
+    [[ ! -f $FIRST_PROVISIONED ]]
 }
 
 if [[ $ENVIRONMENT == "ci" || $ENVIRONMENT == "dev" ]]; then
@@ -11,6 +14,7 @@ if [[ $ENVIRONMENT == "ci" || $ENVIRONMENT == "dev" ]]; then
 fi
 if [[ $ENVIRONMENT == demo* ]] && is_first_provision; then
     echo "always site-install on first provision"
+    touch $FIRST_PROVISIONED
     exit 0
 fi
 exit 0
