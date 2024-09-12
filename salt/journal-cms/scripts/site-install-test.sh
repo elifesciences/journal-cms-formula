2b1d5813ea408fbc7f1dbf0c3f7061603704753d
A. K <74007089+soggy-mushroom@users.noreply.github.com>
2024-09-12 11:05:02 +0100

    Modify site intall script to include logic around CI/dev environments

--- salt/journal-cms/scripts/site-install-test.sh
+++ salt/journal-cms/scripts/site-install-test.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 ENVIRONMENT=$1
-if (ci or dev ) 
-    Alsways site-install    
+if [[ $ENVIRONMENT == "ci" || $ENVIRONMENT == "dev" ]]; then
+    echo "Always site-install" 
     exit 0
-if starts with demo and first previsioned
-    always site-install
-    exit 0
-Exit 0 
+fi
+# if starts with demo and first previsioned
+#     echo "always site-install"
+#     exit 0
+# fi
+exit 0 
