043b5aa40f559908c2fe946d0df2d9efcf434f1f
Nathan Lisgo <nathan@lisgo.co.uk>
2024-09-12 11:57:23 +0100

    Improve observability by logging detected state elifesciences/issues#8678

--- salt/journal-cms/scripts/site-install-test.sh
+++ salt/journal-cms/scripts/site-install-test.sh
@@ -9,12 +9,15 @@ is_first_provision() {
 }
 
 if [[ $ENVIRONMENT == "ci" || $ENVIRONMENT == "dev" ]]; then
+    echo "Environment is ci or dev. Running site-install."
     ../vendor/bin/drush site-install minimal --existing-config -y
     exit 0
 fi
 if [[ $ENVIRONMENT == demo* ]] && is_first_provision; then
+    echo "Environment is demo* and hasn't been provisioned. Running site-install."
     ../vendor/bin/drush site-install minimal --existing-config -y
     date > "$FIRST_PROVISIONED"
     exit 0
 fi
+echo "Skipping site-install (env: $ENVIRONMENT)"
 exit 0
